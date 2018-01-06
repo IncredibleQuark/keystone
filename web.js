776f69532370c81a9ca0a82a681824501663de7b
IncredibleQuark <lkupinski@gmail.com>
2018-01-06 02:02:07 +0100

    basic structure completed

---
+++ web.js
@@ -0,0 +1,27 @@
+var keystone = require('keystone');
+keystone.init({
+
+  'name': 'My Project',
+
+  'favicon': 'public/favicon.ico',
+  'less': 'public',
+  'static': ['public'],
+
+  'views': 'templates/views',
+  'view engine': 'pug',
+
+  'auto update': true,
+  'mongo': 'mongodb://localhost/keystone',
+
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': '(your secret here)'
+
+});
+
+require('./models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
